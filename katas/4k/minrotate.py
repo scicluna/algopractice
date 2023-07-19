bb3740ee4cca7a45163a738d02cf3022ff47c58e
scicluna <88948700+scicluna@users.noreply.github.com>
2023-07-19 14:25:38 -0400

    some more binary search practice

---
+++ katas/4k/minrotate.py
@@ -0,0 +1,46 @@
+# Suppose an array of length n sorted in ascending order is rotated between 1 
+# and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+# [4,5,6,7,0,1,2] if it was rotated 4 times.
+# [0,1,2,4,5,6,7] if it was rotated 7 times.
+# Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+# Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+# You must write an algorithm that runs in O(log n) time.Example 1:
+
+# Input: nums = [3,4,5,1,2]
+# Output: 1
+# Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+
+# Input: nums = [4,5,6,7,0,1,2]
+# Output: 0
+# Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+# Example 3:
+
+# Input: nums = [11,13,15,17]
+# Output: 11
+# Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+
+#plan -> check mid-point. if right is lower we rotate once.
+
+#works, but slow cuz array manip
+# def findMin(nums:list[int])->int:
+#     left = 0
+#     right = len(nums)-1
+#     mid = (left+right)//2
+#     while (nums[mid] > nums[right] or nums[left] > nums[mid]):
+#         nums.insert(0,nums.pop())
+#     return nums[left]
+# print(findMin([3,4,5,1,2]))
+
+#real version
+def findMin(nums:list[int])->int:
+    left, right = 0, len(nums)-1
+    while left < right:
+        mid = (left+right)//2
+        if nums[mid] > nums[right]:
+            left = mid+1
+        else:
+            right = mid
+    return nums[left]
